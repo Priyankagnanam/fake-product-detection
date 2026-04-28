78f9aca66d4c1a06cc912ec0403631ab0b1059a9
Priyankagnanam <gpriyanka17052006@gmail.com>
2026-04-28 21:58:05 +0530

    Update API_URL to use production endpoint

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,3 +1,3 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL || 'https://fake-product-detection-2-bbe7.onrender.com';
 
 export default API_URL;
