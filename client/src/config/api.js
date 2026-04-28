882fb3b8b296fcd36d55862bd675c3d628a84fb0
Priyankagnanam <gpriyanka17052006@gmail.com>
2026-04-28 22:42:06 +0530

    Update api.js

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,3 +1,2 @@
-const API_URL = process.env.REACT_APP_API_URL || 'https://fake-product-detection-2-bbe7.onrender.com';
-
+const API_URL = "https://fake-product-detection-2-bbe7.onrender.com";
 export default API_URL;
